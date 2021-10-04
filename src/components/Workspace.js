d34c17338334870e0f27e1f48551370b091cb95b
hongcheng li <lili09111234@gmail.com>
2021-10-03 21:50:59 -0400

    achieve load top-5list

--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -2,18 +2,47 @@ import React from "react";
 
 export default class Workspace extends React.Component {
     render() {
-        return (
-            <div id="top5-workspace">
-                <div id="workspace-edit">
-                    <div id="edit-numbering">
-                        <div className="item-number">1.</div>
-                        <div className="item-number">2.</div>
-                        <div className="item-number">3.</div>
-                        <div className="item-number">4.</div>
-                        <div className="item-number">5.</div>
+        const {currentList} = this.props;
+        if(currentList!=null){
+            return (
+                <div id="top5-workspace">
+                    <div id="workspace-edit">
+                        <div id="edit-numbering">
+                            <div className="item-number">1.</div>
+                            <div className="item-number">2.</div>
+                            <div className="item-number">3.</div>
+                            <div className="item-number">4.</div>
+                            <div className="item-number">5.</div>
+                        </div>
+                        <div id="edit-items">
+                        {
+                                currentList.items.map((item)=>
+                                    (
+                                    <div class="top5-item">{item}</div>
+                                    )
+                                )
+                        }
+                        </div>
                     </div>
                 </div>
-            </div>
-        )
+            )
+        }
+        else{
+            return (
+                <div id="top5-workspace">
+                    <div id="workspace-edit">
+                        <div id="edit-numbering">
+                            <div className="item-number">1.</div>
+                            <div className="item-number">2.</div>
+                            <div className="item-number">3.</div>
+                            <div className="item-number">4.</div>
+                            <div className="item-number">5.</div>
+                        </div>
+                        <div id="edit-items">
+                        </div>
+                    </div>
+                </div>
+                )
+        }
     }
 }
